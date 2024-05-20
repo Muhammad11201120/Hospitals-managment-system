4e70b0d21bd78efbd59e3ef6342d2fbe44568268
Ibrahim <ibrahim.f.alamri@gmail.com>
2024-05-20 21:12:10 +0300

    User_Contact_Cuntry

---
+++ HMS-DataBusinessLayer/clsUser.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMS_DataAccessLayer;
+namespace HMS_BusinessLayer
+{
+    public class clsUser
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int? UserID {private set; get; }
+        public int EmployeeID { set; get; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public bool IsActive { set; get; }
+
+        public clsUser()
+
+        {
+            this.UserID = -1;
+            this.UserName = "";
+            this.Password = "";
+            this.IsActive = true;
+            Mode = enMode.AddNew;
+        }
+
+        private clsUser(int UserID, int EmployeeID, string Username, string Password,
+            bool IsActive)
+
+        {
+            this.UserID = UserID;
+            this.EmployeeID = EmployeeID;
+            this.UserName = Username;
+            this.Password = Password;
+            this.IsActive = IsActive;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewUser()
+        {
+            SqlParameter[] sp = new SqlParameter[4];
+            sp[0] = new SqlParameter("UserName",this.UserName);
+            sp[1] = new SqlParameter("Password", this.Password);
+            sp[2] = new SqlParameter("IsActive", this.IsActive);
+            sp[3] = new SqlParameter("EmployeeID", this.EmployeeID);
+            this.UserID=clsUsersData.AddNewUser(sp);
+            return (this.UserID !=null);
+        }
+        private bool _UpdateUser()
+        {
+            SqlParameter[] sp = new SqlParameter[5];
+            sp[0] = new SqlParameter("UserID", this.UserID);
+            sp[1] = new SqlParameter("EmployeeID", this.EmployeeID);
+            sp[2] = new SqlParameter("UserName", this.UserName);
+            sp[3] = new SqlParameter("Password", this.Password);
+            sp[4] = new SqlParameter("IsActive", this.IsActive);
+            return clsUsersData.UpdateUser(sp);
+        }
+        public static clsUser Find(int UserID)
+        {
+
+            SqlParameter[] sp = new SqlParameter[5];
+            sp[0] = new SqlParameter("UserID",UserID);
+            sp[1] = new SqlParameter("UserName",null);
+            sp[2] = new SqlParameter("Password",null);
+            sp[3] = new SqlParameter("IsActive",null);
+            sp[4] = new SqlParameter("EmployeeID", null);
+            bool IsFound = clsUsersData.FindUser(ref sp);
+            if (IsFound)
+                return new clsUser((int)sp[0].Value, (int)sp[4].Value, (string)sp[1].Value, (string)sp[2].Value, (bool)sp[3].Value);
+            else
+                return null;
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewUser())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateUser();
+
+            }
+
+            return false;
+        }
+
+        public static DataTable GetAllUsers()
+        {
+            return clsUsersData.GetAllUsers();
+        }
+
+        public static bool DeleteUser(int UserID)
+        {
+            SqlParameter sp = new SqlParameter("userID", UserID);
+            return clsUsersData.DeleteUser(sp);
+        }
+
+    }
+}

d8b2febe958d13206146586dddf8e98578a90f00
Abullah <Abdullah.ra238@gmail.com>
2024-05-21 07:16:48 +0300

    Fix issue

--- HMS-DataBusinessLayer/clsDoctor.cs
+++ HMS-DataBusinessLayer/clsDoctor.cs
@@ -9,108 +9,109 @@ using System.Threading.Tasks;
 
 namespace HMS_DataBusinessLayer
 {
-    //public class clsDoctor 
-    //{
-    //    enum _enMode
-    //    {
-    //        ADD = 1,
-    //        UPDATE = 2
-    //    }
-    //    public int? DoctorID { get; set; }
-    //    public int? EmployeeID { get; set; }
-    //    public int? SpecialtyID { get; set; }
-    //    public decimal? Price { get; set; }
 
-    //    _enMode _Mode = _enMode.ADD;
-    //    public clsDoctor()
-    //    {
-    //        DoctorID = null;
-    //        EmployeeID = null;
-    //        Price = null;
-    //        SpecialtyID = null;
-    //        _Mode = _enMode.ADD;
-    //    }
-    //    public clsDoctor(int ? doctorID ,int? employeeID,
-    //        decimal? price,int? specialtyID )
-    //    {
-    //        DoctorID = doctorID;
-    //        EmployeeID = employeeID;
-    //        Price = price;
-    //        SpecialtyID = specialtyID;
-    //        _Mode = _enMode.UPDATE;
-    //    }
-    //    public static DataTable GetAllEmployees()
-    //    {
-    //        return clsDoctorsData.GetAllDoctor();
-    //    }
-    //    public new clsDoctor Find(int? doctorID)
-    //    {
-    //        int? employeeID  = null, specialtyID = null;
-    //        decimal? price = null;
+    public class clsDoctor
+    {
+        enum _enMode
+        {
+            ADD = 1,
+            UPDATE = 2
+        }
+        public int? DoctorID { get; set; }
+        public int? EmployeeID { get; set; }
+        public int? SpecialtyID { get; set; }
+        public decimal? Price { get; set; }
 
-    //        SqlParameter[] parameters = new SqlParameter[4];
-    //        parameters[0] = new SqlParameter("DoctorID", doctorID);
-    //        parameters[1] = new SqlParameter("EmployeeID", employeeID);
-    //        parameters[2] = new SqlParameter("Price", price);
-    //        parameters[3] = new SqlParameter("SpecialtyID", specialtyID);
+        _enMode _Mode = _enMode.ADD;
+        public clsDoctor()
+        {
+            DoctorID = null;
+            EmployeeID = null;
+            Price = null;
+            SpecialtyID = null;
+            _Mode = _enMode.ADD;
+        }
+        public clsDoctor(int? doctorID, int? employeeID,
+            decimal? price, int? specialtyID)
+        {
+            DoctorID = doctorID;
+            EmployeeID = employeeID;
+            Price = price;
+            SpecialtyID = specialtyID;
+            _Mode = _enMode.UPDATE;
+        }
+        public static DataTable GetAllEmployees()
+        {
+            return clsDoctorsData.GetAllDoctor();
+        }
+        public new clsDoctor Find(int? doctorID)
+        {
+            int? employeeID = null, specialtyID = null;
+            decimal? price = null;
 
-    //        if (clsDoctorsData.Find(ref parameters))
-    //                return new clsDoctor(EmployeeID, price, SpecialtyID);
-    //            else
-    //                return null;
-    //    }
-    //    private bool _AddNewEmployee()
-    //    {
-    //        SqlParameter[] parameters = new SqlParameter[3];
-    //        parameters[0] = new SqlParameter("EmployeeID", this.EmployeeID);
-    //        parameters[1] = new SqlParameter("Price", this.Price);
-    //        parameters[2] = new SqlParameter("SpecialtyID", this.SpecialtyID);
+            SqlParameter[] parameters = new SqlParameter[4];
+            parameters[0] = new SqlParameter("DoctorID", doctorID);
+            parameters[1] = new SqlParameter("EmployeeID", employeeID);
+            parameters[2] = new SqlParameter("Price", price);
+            parameters[3] = new SqlParameter("SpecialtyID", specialtyID);
 
-    //        int? employeeID = clsDoctorsData.AddNew(parameters);
-    //        return employeeID.HasValue;
-    //    }
-    //    private bool _UpdateEmployee()
-    //    {
-    //        SqlParameter[] parameters = new SqlParameter[4];
-    //        parameters[0] = new SqlParameter("DoctorID", this.DoctorID);
-    //        parameters[1] = new SqlParameter("EmployeeID", this.EmployeeID);
-    //        parameters[2] = new SqlParameter("Price", this.Price);
-    //        parameters[3] = new SqlParameter("SpecialtyID", this.SpecialtyID);
+            if (clsDoctorsData.Find(ref parameters))
+                return new clsDoctor(doctorID, employeeID, price, specialtyID);
+            else
+                return null;
+        }
+        private bool _AddNewEmployee()
+        {
+            SqlParameter[] parameters = new SqlParameter[3];
+            parameters[0] = new SqlParameter("EmployeeID", this.EmployeeID);
+            parameters[1] = new SqlParameter("Price", this.Price);
+            parameters[2] = new SqlParameter("SpecialtyID", this.SpecialtyID);
 
-    //        return clsDoctorsData.Update(parameters);
-    //    }
-    //    public bool Delete()
-    //    {
-    //        SqlParameter parameter = new SqlParameter("DoctorID", this.DoctorID);
-    //        if (clsDoctorsData.Delete(parameter))
-    //        {
-    //            return true;
-    //        }
-    //        else
-    //        {
-    //            return false;
-    //        }
-    //    }
-    //    public new bool Save()
-    //    {
-    //        bool result = false;
-    //        switch (_Mode)
-    //        {
-    //            case _enMode.ADD:
-    //                if (_AddNewEmployee())
-    //                {
-    //                    _Mode = _enMode.UPDATE;
-    //                    return true;
-    //                }
-    //                else
-    //                    return false;
-    //            case _enMode.UPDATE:
-    //                return _UpdateEmployee();
+             this.DoctorID = clsDoctorsData.AddNew(parameters);
+            return DoctorID.HasValue;
+        }
+        private bool _UpdateEmployee()
+        {
+            SqlParameter[] parameters = new SqlParameter[4];
+            parameters[0] = new SqlParameter("DoctorID", this.DoctorID);
+            parameters[1] = new SqlParameter("EmployeeID", this.EmployeeID);
+            parameters[2] = new SqlParameter("Price", this.Price);
+            parameters[3] = new SqlParameter("SpecialtyID", this.SpecialtyID);
 
-    //        }
-    //        return result;
-    //    }
-    //}
+            return clsDoctorsData.Update(parameters);
+        }
+        public bool Delete()
+        {
+            SqlParameter parameter = new SqlParameter("DoctorID", this.DoctorID);
+            if (clsDoctorsData.Delete(parameter))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public new bool Save()
+        {
+            bool result = false;
+            switch (_Mode)
+            {
+                case _enMode.ADD:
+                    if (_AddNewEmployee())
+                    {
+                        _Mode = _enMode.UPDATE;
+                        return true;
+                    }
+                    else
+                        return false;
+                case _enMode.UPDATE:
+                    return _UpdateEmployee();
+
+            }
+            return result;
+        }
+    }
 
 
 }

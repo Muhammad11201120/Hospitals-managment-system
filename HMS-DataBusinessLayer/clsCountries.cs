2af46117b7b5e26480b2953a11068ebb74293398
Abullah <Abdullah.ra238@gmail.com>
2024-05-24 09:25:47 +0300

    Add 2 classes in businees layer

--- HMS-DataBusinessLayer/clsCountries.cs
+++ HMS-DataBusinessLayer/clsCountries.cs
@@ -4,36 +4,36 @@ using System.Data.SqlClient;
 
 namespace HMS_DataBusinessLayer
 {
-    public class clsCoutries
+    public class clsCountries
     {
         public int CountryID { private set; get; }
         public string CountryName { set; get; }
-        public clsCoutries()
+        public clsCountries()
         {
             CountryID = -1;
             CountryName = "";
         }
-        private clsCoutries(int CountryID, string CountryName)
+        private clsCountries(int CountryID, string CountryName)
         {
             this.CountryID = CountryID;
             this.CountryName = CountryName;
         }
-        public static clsCoutries GetCountry(int CountryID)
+        public static clsCountries GetCountry(int CountryID)
         {
             SqlParameter[] parameters = new SqlParameter[2];
             parameters[0] = new SqlParameter("CountryID",CountryID);
             parameters[1] = new SqlParameter("CountryName", null);
             if ( clsCoutriesData.GetCountry(ref parameters))
-                return new clsCoutries((int)parameters[0].Value,parameters[1].Value.ToString());
+                return new clsCountries((int)parameters[0].Value,parameters[1].Value.ToString());
             else return null;
         }
-        public static clsCoutries GetCountry(string CountryName)
+        public static clsCountries GetCountry(string CountryName)
         {
             SqlParameter[] parameters = new SqlParameter[2];
             parameters[0].ParameterName = "CountryName";
             parameters[1].Value = CountryName;
             if (clsCoutriesData.GetCountry(ref parameters))
-                return new clsCoutries((int)parameters[0].Value, parameters[1].Value.ToString());
+                return new clsCountries((int)parameters[0].Value, parameters[1].Value.ToString());
             else return null;
         }
         public static DataTable GetAllCountries()

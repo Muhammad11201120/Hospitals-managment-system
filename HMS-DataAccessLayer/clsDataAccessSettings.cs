cf497bdedc6ca54490edfe72acf79a9b113eb34c
Abullah <Abdullah.ra238@gmail.com>
2024-06-11 03:31:38 +0300

    fix files

--- HMS-DataAccessLayer/clsDataAccessSettings.cs
+++ HMS-DataAccessLayer/clsDataAccessSettings.cs
@@ -5,8 +5,8 @@ namespace HMS_DataAccessLayer
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString =
-            "Data Source=SQL8006.site4now.net;Initial Catalog=db_aa9cf8_hospital;User Id=db_aa9cf8_hospital_admin;Password=sa123456";
+        public static string ConnectionString = "";
+
     }
 
 

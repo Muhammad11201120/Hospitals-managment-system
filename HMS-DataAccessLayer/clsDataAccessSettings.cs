4413a92cf836cc4d888a5b0347527b5fbe5d043f
Osama Abdulaziz <Osama7.ya@gmail.com>
2024-06-11 16:13:09 +0300

    Add App.Config

--- HMS-DataAccessLayer/clsDataAccessSettings.cs
+++ HMS-DataAccessLayer/clsDataAccessSettings.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Configuration;
 
 
 namespace HMS_DataAccessLayer
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Data Source=SQL8006.site4now.net;Initial Catalog=db_aa9cf8_hospital;User Id=db_aa9cf8_hospital_admin;Password=sa123456";
+        public static string ConnectionString = Properties.Settings.Default.ConnectionString;
     }
 
 
